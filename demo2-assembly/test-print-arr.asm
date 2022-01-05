f9609b0b73dfda1b41a228f378d3187e359c775b
dingjing <dingjing@live.cn>
2022-01-05 16:48:25 +0800

    数组打印 ok

--- demo2-assembly/test-print-arr.asm
+++ demo2-assembly/test-print-arr.asm
@@ -1,19 +1,29 @@
-.section .data
+.data
     arr: .int 3, 2, 1, 4, 5, 6
     arr_len: .int 6
 
 .extern print_arr_int
 
+.text
 .global _start
 
 _start:
     pushq %rbp
     movq %rsp, %rbp
 
-    movl arr_len, %esi
-    movl arr, %edi
+    # 参数需要压栈
+    subq $16, %rsp
+    movl %edi, -4(%rbp)
+    movq %rsi, -16(%rbp)
+
+    # 参数传递顺序: RID  RSI  RDX  RCX  R8  R9  XMM0-7
+    movl arr_len(%rip), %eax
+    movl %eax, %esi
+    leaq arr(%rip), %rax
+    movq %rax, %rdi
 
     call print_arr_int
 
-    leave
-    ret
+    movq $60, %rax
+    syscall
+    
