b3ad531c1259bd70e0ba1a17e677bf585cefd533
dingjing <dingjing@live.cn>
2022-01-05 17:02:27 +0800

    收尾

--- demo2-assembly/test-print-arr.asm
+++ demo2-assembly/test-print-arr.asm
@@ -19,10 +19,13 @@ _start:
     # 参数传递顺序: RID  RSI  RDX  RCX  R8  R9  XMM0-7
     movl arr_len(%rip), %eax
     movl %eax, %esi
-    leaq arr(%rip), %rax
-    movq %rax, %rdi
+    leaq arr(%rip), %rax        # 需要注意: lea 表示把 内存地址直接给 AX 寄存器
+    movq %rax, %rdi             # mov 表示把地址对应的内容给到 DI 寄存器
 
     call print_arr_int
+
+    nop
+    popq %rbp
 
     movq $60, %rax
     syscall
