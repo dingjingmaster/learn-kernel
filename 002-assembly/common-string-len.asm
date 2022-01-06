6e8a115e82cbfb6a5375fcb2fc8c7206eae7ecae
dingjing <dingjing@live.cn>
2022-01-06 09:49:06 +0800

    更新项目结构

---
+++ 002-assembly/common-string-len.asm
@@ -0,0 +1,41 @@
+.data
+
+.macro pushaq
+    push %rax
+    push %rcx
+    push %rdx
+    push %rbx
+    push %rbp
+    push %rsi
+    push %rdi
+.endm # pushaq
+
+.macro popaq
+    pop %rdi
+    pop %rsi
+    pop %rbp
+    pop %rbx
+    pop %rdx
+    pop %rcx
+    pop %rax
+.endm # popaq
+
+.extern print_int
+
+stringLiteral:     .ascii "This string has\na line break in it.\0"
+.equ len, (. - stringLiteral)
+
+.text
+.global _start
+
+_start:
+    # 打印字符串长度应该是: 35
+    pushaq
+    movq len(%rip), %rdi
+    call print_int
+    popaq
+
+    mov $60, %rax
+    xor %rdi, %rdi
+    syscall
+.end
