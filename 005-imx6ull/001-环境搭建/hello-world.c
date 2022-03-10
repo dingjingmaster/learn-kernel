fcf6a80054e58d1752adfa0350e50ec8dd0ad9b6
dingjing <dingjing@live.cn>
2022-03-10 10:44:59 +0800

    跨平台编译

---
+++ 005-imx6ull/001-环境搭建/hello-world.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+> FileName: hello-world.c
+> Author  : DingJing
+> Mail    : dingjing@live.cn
+> Created Time: 2022年03月09日 星期三 11时13分19秒
+ ************************************************************************/
+#include <stdio.h>
+
+int main (int argc, char* argv[])
+{
+    printf ("Hello World!\n");
+
+    return 0;
+}
